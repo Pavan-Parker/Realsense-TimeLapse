90028e56dc31a2c069dedc5b3e3cb99a9af27f64
Pavan-Parker <ppavanrockz@gmail.com>
2021-06-20 14:27:22 +0530

    --- Auto-Git Commit ---

--- DSLR_TimeLapse.py
+++ DSLR_TimeLapse.py
@@ -9,31 +9,24 @@ import pyrealsense2 as rs
 # Configuration
 N_PHOTOS = 10
 LAPSE = 1
-CAMERA_DELAY = 0  # Canon EOS 1200D delay
-IMG_PATH = "~/Pictures/"
-VID_PATH = "~/Pictures/video.avi"
+CAMERA_DELAY = 0
+IMG_PATH = "/home/iiitb/Pictures/"
+VID_PATH = "/home/iiitb/Pictures/video.avi"
 
 
 def configured_camera():
-    # # Getting the camera
-    # context = gp.Context()
-    # cam = gp.Camera()
-    # cam.init(context)
-
-    # print(cam.get_summary(context))
-    # return cam
-        # Create a pipeline
-##########
+ 
     config = rs.config()
-    config.enable_stream(rs.stream.infrared,1, 848, 480, rs.format.y8, 60)
-    config.enable_stream(rs.stream.depth, 848, 480, rs.format.z16, 60)
+        
+    config.enable_stream(rs.stream.depth, 1280 , 720, rs.format.z16, 6)
+    config.enable_stream(rs.stream.color, 1920 , 1080, rs.format.bgr8, 8)
 
     pipeline = rs.pipeline()
     profile = pipeline.start(config)
 
     sensor = profile.get_device().query_sensors()[0]
     sensor.set_option(rs.option.hdr_enabled, 1)
-##########
+
     # Getting the depth sensor's depth scale (see rs-align example for explanation)
     depth_sensor = profile.get_device().first_depth_sensor()
     depth_scale = depth_sensor.get_depth_scale()
@@ -45,11 +38,12 @@ def configured_camera():
     align_to = rs.stream.color
     align = rs.align(align_to)
     return pipeline,align
+
 def take_photos():
     pipeline,align = configured_camera()
     ctr=0
     for ctr in range(N_PHOTOS):
-
+        
         frames = pipeline.wait_for_frames()
         # frames.get_depth_frame() is a 640x360 depth image
 
@@ -68,33 +62,28 @@ def take_photos():
 
         target = os.path.join(IMG_PATH,ctr)
         print('Copying image to', target)
-        cv2.imwrite(color_img_path, color_img) 
-cv2.imwrite(depth_img_path), depth_img)
-        camera_file = gp.check_result(gp.gp_camera_file_get(cam,
-                                      file_path.folder,
-                                      file_path.name,
-                                      gp.GP_FILE_TYPE_NORMAL))
-
-        gp.check_result(gp.gp_file_save(camera_file, target))
+        cv2.imwrite(IMG_PATH+'_rgb.png', color_frame) 
+        cv2.imwrite(IMG_PATH+'_depth.png', aligned_depth_frame)
         time.sleep(lapse_time())
 
-    gp.check_result(gp.gp_camera_exit(cam))
-
+    pipeline.stop()
 
 def create_video():
-    images = [img for img in os.listdir(IMG_PATH) if img.endswith(".jpg")]
+
+    images = [img for img in os.listdir(IMG_PATH) if img.endswith("_rgb.png")]
+    
     frame = cv2.imread(os.path.join(IMG_PATH, images[0]))
     height, width, layers = frame.shape
 
     video = cv2.VideoWriter(VID_PATH, 0, 24, (width, height))
 
-    print "Creating the video"
+    print("Creating the video")
     for image in images:
         video.write(cv2.imread(os.path.join(IMG_PATH, image)))
 
     cv2.destroyAllWindows()
     video.release()
-    print "Video Created"
+    print("Video Created")
 
 
 # Calculate the time between photos taking into account the camera delay
